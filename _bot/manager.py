44a595a2f8b4c335f75886d1a60724ff13f6dc86
nullmatawasoradesu <119107850+wakaree@users.noreply.github.com>
2023-05-21 19:31:52 +0300

    Update manager.py

--- _bot/manager.py
+++ _bot/manager.py
@@ -10,16 +10,12 @@ from .response import Response
 class RequestManager:
     def __init__(self, bot: Bot, token: str):
         self.bot = bot
-        self.server = bot.server
         self.__token = token
-        self.proxy = bot.proxy
-        self.proxy_auth = bot.proxy_auth
-        self.timeout = bot.timeout
 
     async def __call__(self, method: str, data: Any = None, files: Any = None, **kwargs) -> Response:
         session = await self.bot.get_session()
 
         return await make_request(
-            session, self.server, self.__token, method, data, files,
-            proxy=self.proxy, proxy_auth=self.proxy_auth, timeout=self.timeout, **kwargs
+            session, self.bot.server, self.__token, method, data, files,
+            proxy=self.bot.proxy, proxy_auth=self.bot.proxy_auth, timeout=self.bot.timeout, **kwargs
         )

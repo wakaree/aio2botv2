794ba9d706506198cad18d9e06693c4fa13833db
nullmatawasoradesu <119107850+wakaree@users.noreply.github.com>
2023-05-21 19:30:27 +0300

    Update request_middleware.py

--- _bot/request_middleware.py
+++ _bot/request_middleware.py
@@ -1,17 +1,15 @@
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from typing import Any
 
 from .response import Response
 from .manager import RequestManager
 
 
-class BaseRequestMiddleware:
+class BaseRequestMiddleware(ABC):
     @abstractmethod
     async def __call__(
         self, request: RequestManager, method: str,
         data: Any = None, files: Any = None, **kwargs: Any
     ) -> Response:
-        raise NotImplementedError(
-            f'{self.__class__.__name__} must implement __call__ method!'
-        )
+        ...
